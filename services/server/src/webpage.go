d0aeffbf9e3e90a0e8c910885944b8ce73c0462c
vseipelt <victor.seipelt@gmail.com>
2026-03-22 01:18:56 +0100

    Split web page parsing loop into smaller maintainable subfunctions with a single responsibility

--- services/server/src/webpage.go
+++ services/server/src/webpage.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 	"strings"
 	"time"
 
@@ -20,103 +21,145 @@ type WebPage struct {
 	RetrievalTime time.Time
 }
 
-func FetchWebPage(targetURL string) (res WebPage, err error) {
+func FetchWebPage(targetURL string) (WebPage, error) {
 	resp, err := http.Get(targetURL)
 	if err != nil {
-		err = fmt.Errorf("FetchWebPage: %w", err)
-		return
+		return WebPage{}, fmt.Errorf("FetchWebPage: %w", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		err = fmt.Errorf("FetchWebPage: getting %q: %s", targetURL, resp.Status)
-		return
+		return WebPage{}, fmt.Errorf("FetchWebPage: getting %q: %s", targetURL, resp.Status)
 	}
 
 	doc, err := html.Parse(resp.Body)
 	if err != nil {
-		err = fmt.Errorf("FetchWebPage: parsing %q as HTML: %w", targetURL, err)
-		return
+		return WebPage{}, fmt.Errorf("FetchWebPage: parsing %q as HTML: %w", targetURL, err)
 	}
 
-	res.URL = resp.Request.URL.String()
-	res.Links = make(map[string]bool)
-	res.RetrievalTime = time.Now()
+	return parseWebPage(doc, resp.Request.URL), nil
+}
 
+func parseWebPage(doc *html.Node, url *url.URL) WebPage {
+	return WebPage{
+		URL:           url.String(),
+		Title:         extractTitle(doc),
+		Description:   extractDescription(doc),
+		Content:       extractContent(doc),
+		Links:         extractLinks(doc, url),
+		RetrievalTime: time.Now(),
+	}
+}
+
+func extractDescription(doc *html.Node) string {
 	for n := range doc.Descendants() {
-		// skip script and style
-		if n.Type == html.ElementNode && (n.DataAtom == atom.Script || n.DataAtom == atom.Style) {
+		if n.Type != html.ElementNode || n.DataAtom != atom.Meta {
 			continue
 		}
 
-		// meta data
-		if n.Type == html.ElementNode && n.DataAtom == atom.Title {
-			if n.FirstChild != nil && n.FirstChild.Type == html.TextNode {
-				var text = strings.TrimSpace(n.FirstChild.Data)
-				if text != "" {
-					res.Title = text
-				}
+		found := false
+		description := ""
+		for _, a := range n.Attr {
+			if a.Key == "property" && a.Val == "og:description" ||
+				a.Key == "name" && a.Val == "description" {
+
+				found = true
+			} else if a.Key == "content" {
+				description = a.Val
 			}
 		}
-
-		if n.Type == html.ElementNode && n.DataAtom == atom.Meta {
-			var target = ""
-			var content = ""
-			for _, meta := range n.Attr {
-				if meta.Key == "property" && meta.Val == "og:title" {
-					target = "title"
-				}
-
-				if meta.Key == "name" && meta.Val == "description" {
-					target = "description"
-				}
-				if meta.Key == "property" && meta.Val == "og:description" {
-					target = "description"
-				}
-
-				if meta.Key == "content" {
-					content = meta.Val
-				}
-			}
-
-			switch target {
-			case "title":
-				res.Title = content
-			case "description":
-				res.Description = content
-			}
-		}
-
-		// content
-		if n.Type == html.TextNode {
-			var text = strings.TrimSpace(n.Data)
-			if text != "" {
-				res.Content += text
-			}
-		}
-
-		// links
-		if n.Type == html.ElementNode && n.DataAtom == atom.A {
-			for _, a := range n.Attr {
-				if a.Key == "href" {
-					var rawUrl = a.Val
-					if !strings.HasPrefix(a.Val, "http://") && !strings.HasPrefix(a.Val, "https://") {
-						rawUrl = res.URL + "/" + a.Val
-					}
-					var normalizedUrl, error = urlx.NormalizeString(rawUrl)
-					if error != nil {
-						continue
-					}
-
-					if !res.Links[normalizedUrl] {
-						res.Links[normalizedUrl] = true
-					}
-
-					break
-				}
-			}
+		if found {
+			return description
 		}
 	}
 
-	return
+	// not found
+	return ""
+}
+
+func extractTitle(doc *html.Node) string {
+	// try: open graph meta ("og:title")
+	for n := range doc.Descendants() {
+		if n.Type != html.ElementNode || n.DataAtom != atom.Meta {
+			continue
+		}
+
+		found := false
+		title := ""
+		for _, a := range n.Attr {
+			if a.Key == "property" && a.Val == "og:title" {
+				found = true
+			} else if a.Key == "content" {
+				title = a.Val
+			}
+		}
+		if found {
+			return title
+		}
+	}
+
+	// try: title tag
+	for n := range doc.Descendants() {
+		if n.Type == html.ElementNode && n.DataAtom == atom.Title && n.FirstChild != nil {
+			return n.FirstChild.Data
+		}
+	}
+
+	// no title found
+	return ""
+}
+
+func extractContent(doc *html.Node) string {
+	var sb strings.Builder
+
+	// use recursion here so we can skip style and script element nodes and
+	// all of their children, which Node.Descendents will not do.
+	var inner func(n *html.Node)
+	inner = func(n *html.Node) {
+		if n.Type == html.TextNode {
+			text := strings.TrimSpace(n.Data)
+			if text != "" {
+				sb.WriteString(text)
+				sb.WriteByte(' ')
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			if c.Type == html.ElementNode &&
+				(c.DataAtom == atom.Style || c.DataAtom == atom.Script) {
+				continue
+			}
+			inner(c)
+		}
+	}
+	inner(doc)
+
+	return strings.TrimSpace(sb.String()) // remove trailing whitespace
+}
+
+func extractLinks(doc *html.Node, url *url.URL) map[string]bool {
+	links := make(map[string]bool)
+	for n := range doc.Descendants() {
+		if n.Type != html.ElementNode || n.DataAtom != atom.A {
+			continue
+		}
+		for _, a := range n.Attr {
+			if a.Key != "href" {
+				continue
+			}
+
+			link, err := url.Parse(a.Val)
+			if err != nil {
+				continue // ignore error
+			}
+			normalizedLink, err := urlx.Normalize(link)
+			if err != nil {
+				continue // ignore error
+			}
+
+			if !links[normalizedLink] {
+				links[normalizedLink] = true
+			}
+		}
+	}
+	return links
 }
